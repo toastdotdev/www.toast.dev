4acca9f4275fd0d9c2aec0e1f042cf9a7e8c7c84
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-10-03 17:30:29 -0700

    feat: adds path to check for purgeCSS

--- postcss-config/tailwind.config.js
+++ postcss-config/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./src/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
